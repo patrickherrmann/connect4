025a39d8708c10589752a3f9acbc667a44a14b48
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-03-09 00:10:53 -0700

    Add robust command line interface

--- Main.hs
+++ Main.hs
@@ -1,23 +1,60 @@
 import ConnectFour
 import Data.List
+import Options.Applicative
 
 data GameConfig = GameConfig
    { winLength :: Int
    , rows :: Int
    , cols :: Int
-   , startingPlayer :: Color
-   , showBoard :: Board -> String
+   , textMode :: TextMode
    }
 
-initialState :: GameConfig -> GameState
-initialState config = GameState board (startingPlayer config)
-    where board = createBoard (rows config, cols config)
+data TextMode = Unicode | Ascii
+
+parseWinLength :: Parser Int
+parseWinLength = option auto
+    $  long "connect"
+    <> metavar "N"
+    <> value 4
+    <> help "The number of pieces to connect to achieve victory"
+
+parseRows :: Parser Int
+parseRows = option auto
+    $  long "rows"
+    <> short 'r'
+    <> metavar "ROWS"
+    <> value 6
+    <> help "The number of rows for the board"
+
+parseCols :: Parser Int
+parseCols = option auto
+    $  long "cols"
+    <> short 'c'
+    <> metavar "COLS"
+    <> value 7
+    <> help "The number of columns for the board"
+
+parseTextMode :: Parser TextMode
+parseTextMode = flag Unicode Ascii
+    $  long "ascii"
+    <> help "Draw an ascii board instead of unicode"
+
+parseGameConfig :: Parser GameConfig
+parseGameConfig = GameConfig
+    <$> parseWinLength
+    <*> parseRows
+    <*> parseCols
+    <*> parseTextMode
+
+showBoard :: TextMode -> Board -> String
+showBoard Unicode = showBoardUnicode
+showBoard Ascii = showBoardAscii
 
 parseColumn :: Board -> String -> Maybe Int
-parseColumn _ "" = Nothing
-parseColumn b (x:_) = do
+parseColumn b [x] = do
     i <- elemIndex x (columnNames b)
     return $ i + 1
+parseColumn _ _ = Nothing
 
 performGameStep :: GameState -> IO GameState
 performGameStep gs@(GameState b color) = do
@@ -34,22 +71,24 @@ performGameStep gs@(GameState b color) = do
             Just gs' -> return gs'
 
 playGame :: GameConfig -> GameState -> IO ()
-playGame config gs@(GameState b color) = do
-    putStrLn ""
-    putStr . showBoard config $ b
-    if gameOver gs 4
+playGame conf gs@(GameState b color) = do
+    putStr "\n\n\n"
+    putStr . showBoard (textMode conf) $ b
+    putStr "\n"
+    if gameOver gs $ winLength conf
         then do
             putStrLn "Game over!"
             putStrLn $ show (opponent color) ++ " wins."
-        else performGameStep gs >>= playGame config
+        else performGameStep gs >>= playGame conf
+
+start :: GameConfig -> IO ()
+start c = playGame c startingState
+    where startingState =
+            GameState (createBoard (rows c, cols c)) White
 
 main :: IO ()
-main = do
-    let config = GameConfig {
-        showBoard = showBoardUnicode,
-        winLength = 4,
-        rows = 6,
-        cols = 7,
-        startingPlayer = White
-    }
-    playGame config (initialState config)
+main = execParser opts >>= start
+  where opts = info (helper <*> parseGameConfig)
+          ( fullDesc
+         <> header "connect4 - \
+            \Play connect 4 from a command line interface" )
