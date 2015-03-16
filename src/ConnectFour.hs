1c5cc9b45fa3090facea0ad63672af719047dfe8
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-03-16 00:44:57 -0700

    Formatting

--- src/ConnectFour.hs
+++ src/ConnectFour.hs
@@ -3,8 +3,8 @@ module ConnectFour
 , Col
 , Loc
 , Cell
-, Color(..)
 , Board
+, Color(..)
 , GameState(..)
 , Status(..)
 , opponent
@@ -33,9 +33,9 @@ type Col = Int
 type Loc = (Row, Col)
 type Cell = Maybe Color
 
-data Color = Black | White deriving (Show, Eq)
-
 type Board = Array Loc Cell
+
+data Color = Black | White deriving (Show, Eq)
 
 data GameState = GameState Board Status deriving (Show)
 
@@ -60,17 +60,17 @@ minimax s pro d gs@(GameState b (Undecided c)) =
 
 unsafeMove :: Int -> GameState -> Int -> GameState
 unsafeMove s (GameState b (Undecided c)) i = GameState b' status
-    where status
-            | gameOver b' s = Winner c
-            | null (validMoves b') = Draw
-            | otherwise = Undecided $ opponent c
-          b' = dropPiece b i c
+  where status
+          | gameOver b' s = Winner c
+          | null (validMoves b') = Draw
+          | otherwise = Undecided $ opponent c
+        b' = dropPiece b i c
 unsafeMove _ _ _ = error "Cannot perform moves on decided boards"
 
 move :: Int -> GameState -> Int -> Maybe GameState
 move s gs@(GameState b _) i = if i `elem` validMoves b
-    then Just $ unsafeMove s gs i
-    else Nothing
+  then Just $ unsafeMove s gs i
+  else Nothing
 
 opponent :: Color -> Color
 opponent Black = White
@@ -81,11 +81,11 @@ createBoard ub = listArray ((1, 1), ub) $ repeat Nothing
 
 colCount :: Board -> Int
 colCount b = cs
-    where (_, (_, cs)) = bounds b
+  where (_, (_, cs)) = bounds b
 
 rowCount :: Board -> Int
 rowCount b = rs
-    where (_, (rs, _)) = bounds b
+  where (_, (rs, _)) = bounds b
 
 columnFull :: Board -> Int -> Bool
 columnFull b c = isJust (b ! (1, c))
@@ -98,22 +98,22 @@ setPiece b l c = b // [(l, c)]
 
 dropPiece :: Board -> Int -> Color -> Board
 dropPiece b i color = setPiece b (last open) (Just color)
-    where (open, _) = break (isJust . (b !))
-                    . filter sameCol $ indices b
-          sameCol (_, c) = c == i
+  where (open, _) = break (isJust . (b !))
+                  . filter sameCol $ indices b
+        sameCol (_, c) = c == i
 
 groups :: (Ord a, Eq a) => Board -> (Loc -> a) -> [[Cell]]
 groups b grouping = map (map (b !)) inds
-    where inds = groupBy ((==) `on` grouping)
-                . sortBy (comparing grouping)
-                $ indices b
+  where inds = groupBy ((==) `on` grouping)
+             . sortBy (comparing grouping)
+             $ indices b
 
 vectors :: Board -> [[Cell]]
 vectors b = [diag1, diag2, vert, horiz] >>= groups b
-    where diag1 (r, c) = r - c
-          diag2 (r, c) = r + c
-          vert  (_, c) = c
-          horiz (r, _) = r
+  where diag1 (r, c) = r - c
+        diag2 (r, c) = r + c
+        vert  (_, c) = c
+        horiz (r, _) = r
 
 evalBoard :: Board -> Int
 evalBoard b = sum . map (evalVector . group) $ vectors b
@@ -135,8 +135,8 @@ sign White = 1
 
 gameOver :: Board -> Int -> Bool
 gameOver b streak = any checkLine $ vectors b
-    where checkLine = any checkGroup . group
-          checkGroup g = isJust (head g) && length g >= streak
+  where checkLine = any checkGroup . group
+        checkGroup g = isJust (head g) && length g >= streak
 
 showTileAscii :: Cell -> Char
 showTileAscii Nothing = '.'
@@ -145,11 +145,11 @@ showTileAscii (Just White) = 'X'
 
 showBoardAscii :: Board -> String
 showBoardAscii b = unlines
-            . map (intersperse ' ')
-            . (++ [columnNames b])
-            . chunksOf (colCount b)
-            . map showTileAscii
-            . elems $ b
+                 . map (intersperse ' ')
+                 . (++ [columnNames b])
+                 . chunksOf (colCount b)
+                 . map showTileAscii
+                 . elems $ b
 
 showTileUnicode :: Cell -> Char
 showTileUnicode Nothing = ' '
@@ -162,18 +162,18 @@ format start mid end cells =
 
 showBoardUnicode :: Board -> String
 showBoardUnicode b = allRows ++ colHeader
-    where cols = colCount b
-          rows = chunksOf cols
-               . map showTileUnicode
-               . elems $ b
-          colHeader = format " " " " " \n" . map pad $ columnNames b
-          topRow = format "╓" "┬" "╖\n" $ perCol "───"
-          midRow = format "╟" "┼" "╢\n" $ perCol "───"
-          botRow = format "╚" "╧" "╝\n" $ perCol "═══"
-          formatRow row = format "║" "│" "║\n" $ map pad row
-          allRows = format topRow midRow botRow $ map formatRow rows
-          perCol = replicate cols
-          pad c = [' ', c, ' ']
+  where cols = colCount b
+        rows = chunksOf cols
+             . map showTileUnicode
+             . elems $ b
+        colHeader = format " " " " " \n" . map pad $ columnNames b
+        topRow = format "╓" "┬" "╖\n" $ perCol "───"
+        midRow = format "╟" "┼" "╢\n" $ perCol "───"
+        botRow = format "╚" "╧" "╝\n" $ perCol "═══"
+        formatRow row = format "║" "│" "║\n" $ map pad row
+        allRows = format topRow midRow botRow $ map formatRow rows
+        perCol = replicate cols
+        pad c = [' ', c, ' ']
 
 columnNames :: Board -> String
 columnNames b = take (colCount b) ['a'..]
--- src/Main.hs
+++ src/Main.hs
@@ -3,57 +3,57 @@ import Data.List
 import Options.Applicative
 
 data GameConfig = GameConfig
-   { winLength :: Int
-   , rows :: Int
-   , cols :: Int
-   , textMode :: TextMode
-   , ai :: Int
-   }
+  { winLength :: Int
+  , rows :: Int
+  , cols :: Int
+  , textMode :: TextMode
+  , ai :: Int
+  }
 
 data TextMode = Unicode | Ascii
 
 parseWinLength :: Parser Int
 parseWinLength = option auto
-    $  long "connect"
-    <> metavar "N"
-    <> value 4
-    <> help "The number of pieces to connect to achieve victory"
+  $  long "connect"
+  <> metavar "N"
+  <> value 4
+  <> help "The number of pieces to connect to achieve victory"
 
 parseRows :: Parser Int
 parseRows = option auto
-    $  long "rows"
-    <> short 'r'
-    <> metavar "ROWS"
-    <> value 6
-    <> help "The number of rows for the board"
+  $  long "rows"
+  <> short 'r'
+  <> metavar "ROWS"
+  <> value 6
+  <> help "The number of rows for the board"
 
 parseCols :: Parser Int
 parseCols = option auto
-    $  long "cols"
-    <> short 'c'
-    <> metavar "COLS"
-    <> value 7
-    <> help "The number of columns for the board"
+  $  long "cols"
+  <> short 'c'
+  <> metavar "COLS"
+  <> value 7
+  <> help "The number of columns for the board"
 
 parseTextMode :: Parser TextMode
 parseTextMode = flag Unicode Ascii
-    $  long "ascii"
-    <> help "Draw an ascii board instead of unicode"
+  $  long "ascii"
+  <> help "Draw an ascii board instead of unicode"
 
-parseAi :: Parser Int
-parseAi = option auto
-    $  long "ai"
-    <> metavar "DIFFICULTY"
-    <> value 4
-    <> help "How difficult to make the computer player"
+parseDifficulty :: Parser Int
+parseDifficulty = option auto
+  $  long "ai"
+  <> metavar "DIFFICULTY"
+  <> value 4
+  <> help "How difficult to make the computer player"
 
 parseGameConfig :: Parser GameConfig
 parseGameConfig = GameConfig
-    <$> parseWinLength
-    <*> parseRows
-    <*> parseCols
-    <*> parseTextMode
-    <*> parseAi
+  <$> parseWinLength
+  <*> parseRows
+  <*> parseCols
+  <*> parseTextMode
+  <*> parseDifficulty
 
 showBoard :: TextMode -> Board -> String
 showBoard Unicode = showBoardUnicode
@@ -68,58 +68,57 @@ showColor conf color = showTile (textMode conf) (Just color)
 
 parseColumn :: Board -> String -> Either String Int
 parseColumn b [x] = case elemIndex x (columnNames b) of
-    Nothing -> Left $ "No column '" ++ [x] ++ "'."
-    Just i  -> Right $ i + 1
+  Nothing -> Left $ "No column '" ++ [x] ++ "'."
+  Just i  -> Right $ i + 1
 parseColumn _ _ = Left "Enter a single key to indicate\
                        \ the column in which to play."
 
 tryMove :: GameConfig -> GameState -> Int -> Either String GameState
 tryMove conf gs col = case move (winLength conf) gs col of
-    Nothing -> Left "The column is full! Try again."
-    Just gs' -> Right gs'
+  Nothing -> Left "The column is full! Try again."
+  Just gs' -> Right gs'
 
 getPlayerInput :: GameConfig -> GameState -> IO GameState
 getPlayerInput conf gs@(GameState b (Undecided color)) = do
-    putStrLn $ showColor conf color : " to play:"
-    input <- getLine
-    let result = do
-            column <- parseColumn b input
-            tryMove conf gs column
-    case result of
-        Left err -> do
-            putStrLn err
-            getPlayerInput conf gs
-        Right gs' -> return gs'
+  putStrLn $ showColor conf color : " to play:"
+  input <- getLine
+  let result = do
+        column <- parseColumn b input
+        tryMove conf gs column
+  case result of
+    Left err -> do
+      putStrLn err
+      getPlayerInput conf gs
+    Right gs' -> return gs'
 getPlayerInput _ _ = error "Cannot perform moves on decided boards"
 
 getAiInput :: GameConfig -> GameState -> IO GameState
 getAiInput conf gs@(GameState _ (Undecided color)) = do
-        putStrLn $ showColor conf color
-            : " plays " ++ [['a'..] !! (m - 1), '!']
-        return gs'
-    where m = bestMove (winLength conf) color (ai conf) gs
-          (Right gs') = tryMove conf gs m
+    putStrLn $ showColor conf color : " plays " ++ [['a'..] !! (m - 1), '!']
+    return gs'
+  where m = bestMove (winLength conf) color (ai conf) gs
+        (Right gs') = tryMove conf gs m
 getAiInput _ _ = error "Cannot perform moves on decided boards"
 
 gameStep :: GameConfig -> GameState -> IO ()
 gameStep conf gs@(GameState b status) = do
-    putStr $ (showBoard $ textMode conf) b
-    case status of
-        Draw -> putStrLn "It's a draw!"
-        Winner w -> putStrLn $ showColor conf w : " wins!"
-        Undecided c -> case c of
-            White -> getPlayerInput conf gs >>= gameStep conf
-            Black -> getAiInput conf gs >>= gameStep conf
+  putStr $ (showBoard $ textMode conf) b
+  case status of
+    Draw -> putStrLn "It's a draw!"
+    Winner w -> putStrLn $ showColor conf w : " wins!"
+    Undecided c -> case c of
+      White -> getPlayerInput conf gs >>= gameStep conf
+      Black -> getAiInput conf gs >>= gameStep conf
 
 play :: GameConfig -> IO ()
 play conf = gameStep conf blank
-    where blank = GameState board (Undecided White)
-          board = createBoard (rows conf, cols conf)
+  where blank = GameState board (Undecided White)
+        board = createBoard (rows conf, cols conf)
 
 main :: IO ()
 main = execParser opts >>= play
   where opts = info (helper <*> parseGameConfig)
-         $  fullDesc
-         <> header "connect4 - \
-                   \Play connect 4 from a command line interface"
-         <> progDesc "Change the connection length and board size"
+             $  fullDesc
+             <> header "connect4 - \
+                       \Play connect 4 from a command line interface"
+             <> progDesc "Change the connection length and board size"
